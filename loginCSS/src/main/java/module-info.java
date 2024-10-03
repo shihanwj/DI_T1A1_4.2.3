b4d36b3a7aaf35eb5c608301aa9b22c0a5a78f9f
shihanwj <shihanwei495@gmail.com>
2024-10-03 10:49:22 +0200

    Delete loginCSS directory

--- loginCSS/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module es.ieslosmontecillos.logincss {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens es.ieslosmontecillos.logincss to javafx.fxml;
-    exports es.ieslosmontecillos.logincss;
-}
